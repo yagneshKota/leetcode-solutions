342bd71247ff78b16096b3d8c821ff10951ece6c
yagneshKota <yagnesh.iiitp@gmail.com>
2026-07-29 23:07:47 +0530

    Time: 35 ms (32.46%), Space: 52 MB (44.94%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.cpp
+++ 0888-fair-candy-swap/0888-fair-candy-swap.cpp
@@ -8,13 +8,12 @@ public:
         for(int i=0; i<n; i++) aliceSum += aliceSizes[i];
         for(int i=0; i<m; i++) bobSum += bobSizes[i];
 
-        int x= (aliceSum- bobSum)/2;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                int diff= aliceSizes[i]- bobSizes[j];
-                if(diff == x ){
-                    return {aliceSizes[i], bobSizes[j]};
-                }
+        int diff= (bobSum- aliceSum)/2;
+        unordered_set<int> set(bobSizes.begin(), bobSizes.end());
+        
+        for (int a : aliceSizes) {
+            if (set.count(a+ diff)) {
+                return {a, a+ diff};
             }
         }
         return {};
